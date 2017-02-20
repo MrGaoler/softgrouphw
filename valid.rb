187a7ddc85ffee63ec1f6048a8dd4c036427ba43
MrGaoler <notwrnglife@gmail.com>
2017-02-21 00:18:36 +0200

    second commit

--- valid.rb
+++ valid.rb
@@ -1,9 +1,9 @@
 class String
 
-  def valid? (str = self.split(//), znach = true)
-    return znach if str.empty?
-    return false if !( str.count % 2 ).zero? || !znach
-    valid? str, ( str.pop.ord - str.shift.ord).between?(1, 2)
+  def valid? (string = self.split(//), znach = true)
+    return znach if string.empty?
+    return false if !( string.count % 2 ).zero? || !znach
+    valid? string, ( string.pop.ord - string.shift.ord).between?(1, 2)
   end
 end
 
