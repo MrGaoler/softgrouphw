759436dda784671f5a1b7579244806e69a7aa272
MrGaoler <notwrnglife@gmail.com>
2017-02-26 16:45:04 +0200

    second commit

--- matrix.rb
+++ matrix.rb
@@ -1,19 +1,3 @@
-# Class Prime_numb
-#   def prime?
-#     n = [].to_a
-#     n[1] = 0
-#       n.each do
-#         return  false  if (n==1)
-#     for (k=2,k<=s,k++)
-#       n[k]=1
-#       for(k=2,k*k<=s,k++)
-#         if(s[k]==1)
-#         for(l=k*k, l<=n,l+=k)
-#         s[l]=0
-#
-#       end
-#     end
-# return n
 
 def eratosthenes(n)
 numbs = Array.[](nil, nil, *2..n)
@@ -21,8 +5,8 @@ numbs = Array.[](nil, nil, *2..n)
 (i**2..n).step(i){|m| numbs[m] = nil} if numbs[i]
 
 end
-@numbs.compact
+numbs.compact
 end
- numb = Array.new(4) { Array.new(@numbs)}
+ numb = Array.new(4) { Array.new(numbs)}
 
 p eratosthenes(10)
