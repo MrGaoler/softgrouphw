fd97cbcfbd4e5677fb934cfc866e30497f28fe66
MrGaoler <notwronglife@gmail.com>
2017-03-07 01:05:16 +0200

    Luke i'm father

---
+++ download.rb
@@ -0,0 +1,24 @@
+require 'faraday'
+require 'faker'
+class Image
+  def self.download(url)
+    response = Faraday.get(url)
+    checker(response)
+    type = Faker::StarWars.character + ".#{url.split('.').last}"
+    type = type.delete(' ')
+    File.open(type, 'wb') { |file| file.write(response.body) }
+  end
+
+  def self.checker(response)
+    raise ArgumentError, 'did u paste proper URL?' if response
+                                                      .status.to_s
+                                                      .scan(/^\d/) == (4 || 5)
+
+    raise TypeError, 'your link doesn`t contain image' if response
+                                                          .headers['Content-Type']
+                                                          .split('/')
+                                                          .first != 'image'
+  end
+end
+
+Image.download('http://www.sunhome.ru/i/common/logo.gif')
